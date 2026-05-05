a5c74a3ec9304cde48ee3d2faef4a8255ee502ea
arjunbhosle888 <arjunbhosale66662@gmail.com>
2026-05-05 18:48:58 +0530

    Create program 1.py

---
+++ Experiment no 1/program 1.py
@@ -0,0 +1,18 @@
+# Program to display personal details
+Created on Thu Apr 2 00:18:53 2026
+@author: arjun bhosale
+name = "arjun bhosale"
+address = "Satara, Maharashtra, India"
+
+10
+
+contact = "93784681296"
+print("name:", name)
+
+13
+
+print(" address:", address)
+
+14
+
+print("contact:", contact)
